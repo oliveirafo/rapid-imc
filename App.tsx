e75d5d34a6a9ba9177634ae95adbad96437e8bc6
oliveirascof <oliveirascof@gmail.com>
2023-05-20 01:17:40 -0300

    feat: add Ad Open App

--- App.tsx
+++ App.tsx
@@ -4,6 +4,15 @@ import { useFonts } from 'expo-font';
 import { VisibilityProvider } from './src/context/visibilitycontext';
 import { useEffect } from 'react';
 import { Update } from './src/services/checkUpdate';
+import { TestIds, AppOpenAd } from 'react-native-google-mobile-ads';
+
+const PUB_OPEN_ID = __DEV__ ? TestIds.APP_OPEN : 'ca-app-pub-2213444535919704/3383426137'
+
+const adOpen = AppOpenAd.createForAdRequest(PUB_OPEN_ID, {
+    requestNonPersonalizedAdsOnly: true,
+    keywords: ['fashion', 'clothing'],
+});
+
 
 
 export default function App (): JSX.Element {
@@ -15,6 +24,13 @@ export default function App (): JSX.Element {
 
   useEffect(() => {
     Update()
+  }, [])
+
+  useEffect(() => {
+    adOpen.load()
+    setTimeout(() => {
+      adOpen.show()
+    }, 1000)
   }, [])
 
   return (
